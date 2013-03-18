1a69ada5bcc8d9fd3ce1a1cab58c7eca64e656c7
Student <csstudent@TRIN-B18-JPW04.local>
2013-03-18 13:15:32 -0400

    added globals class with player members included

---
+++ BattleShip/Globals.h
@@ -0,0 +1,13 @@
+//
+//  Globals.h
+//  BattleShip
+//
+//  Created by Student on 3/15/13.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Globals : NSObject
+
+@end
---
+++ BattleShip/Globals.m
@@ -0,0 +1,13 @@
+//
+//  Globals.m
+//  BattleShip
+//
+//  Created by Student on 3/15/13.
+//
+//
+
+#import "Globals.h"
+
+@implementation Globals
+
+@end
